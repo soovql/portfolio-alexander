6032f9dbc42c69e2e79a16dd46539f17f68fcc1e
ndanilova <ndanilova@ivi.ru>
2021-05-08 00:30:08 +0300

    add mobile gallery, fix dots progress bar

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {useEffect, useState} from "react";
 import { cn } from '@bem-react/classname';
 import Slick, { Settings } from 'react-slick';
+import classNames from "classnames";
 
 type ISliderProps = {
   parentClass?: string;
@@ -21,12 +22,20 @@ const Slider = React.forwardRef<HTMLDivElement, ISliderProps>(function Slider(
   useEffect(() => {
     setTimeout(()=>{
       const active_slide = document.getElementsByClassName('slick-current')[0] as HTMLElement;
+      const active_dot = document.getElementsByClassName('slick-active')[1] as HTMLElement;
       active_slide.classList.add('zoomed');
-    }, 1000)
+      active_dot.classList.add('current');
+    }, 100)
 
   }, [isZoomed])
 
-  const templates = require.context('../../styles/images/gallery/', true, /\.(jpg|jpeg)$/);
+  let templates: any;
+  if (window.innerWidth < 599) {
+    templates = require.context('../../styles/images/gallery/mobile/', true, /\.(jpg|jpeg)$/);
+  } else {
+    templates = require.context('../../styles/images/gallery/desktop/', true, /\.(jpg|jpeg)$/);
+  }
+
 
   const slickSettings: Settings = {
     lazyLoad: 'ondemand',
@@ -34,10 +43,12 @@ const Slider = React.forwardRef<HTMLDivElement, ISliderProps>(function Slider(
     draggable: false,
     dots: !open,
     infinite: true,
-    speed: 800,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 7000,
+    cssEase: 'linear',
+    fade: true,
     autoplay: !open,
     swipeToSlide: true,
     beforeChange: (index: number, next: number) => {
@@ -53,25 +64,26 @@ const Slider = React.forwardRef<HTMLDivElement, ISliderProps>(function Slider(
     },
     customPaging: function customPage2(i) {
       return (
-          <div
+          <a
               className={"customDot"}
           >
-            {i + 1}
-          </div>
+            <div className={"customDotValue"}/>
+              {i + 1}
+          </a>
       )
     }
   };
 
   const renderItems = () => {
-    return  templates.keys().map((elem, i) => (
-        <div key={i}>
-          <img className="slider-image" key={elem} src={templates(elem).default} alt="Саша Стюхин Пейзажист" />
+    return  templates.keys().map((elem: number, i: number) => (
+        <div key={i} className="slider-image-container">
+          <img className="slider-image" key={elem} src={templates(elem).default} height={window.innerHeight} alt="Саша Стюхин Пейзажист" />
         </div>
     ))
   };
 
   return (
-    <div {...rest} className={'slider'} ref={ref}>
+    <div {...rest} className={classNames('slider', open && 'blurred')} ref={ref}>
       <Slick {...slickSettings}>{renderItems()}</Slick>
     </div>
   );
--- src/logoColors.ts
+++ src/logoColors.ts
@@ -1,11 +1,20 @@
 export enum MainPageLogoColors {
-  '#FFFFFF',
-  '#331E19',
-  '#1D455F',
+  '#F5F9FA',
+  '#75697D',
+  '#C6CCE2',
+  '#DDE8F3',
 }
 
 export enum MainPageBurgerColors {
-  '#E5EDEF',
-  '#E0B497',
-  '#60626F',
+  '#95AEB5',
+  '#75697D',
+  '#C6CCE2',
+  '#DDE8F3',
+}
+
+export enum MainPageBurgerLineColors {
+  '#F5F9FA',
+  '#000000',
+  '#C6CCE2',
+  '#DDE8F3',
 }
